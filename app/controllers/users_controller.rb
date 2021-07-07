dcbe21d514d3594812c77c6aa2acfe30d1d20895
Andrei <litvinov.andrey@outlook.com>
2021-07-07 18:00:37 +0300

    edit standard registration pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:profile]
 
+
   def profile
     @email = current_user.email
     @name = current_user.name
@@ -8,9 +9,4 @@ class UsersController < ApplicationController
     @bio = current_user.bio
   end
 
-  private
-
-  def user_params
-    params.require(:user).permit(:id, :email)
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  EMAIL_FORMAT = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
+  has_many :posts
+
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: EMAIL_FORMAT, message: 'invalid email' }
 end
