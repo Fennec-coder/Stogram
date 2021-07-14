c47597b9d50b10b07fad17a7950c038d3b95af8a
Andrei <litvinov.andrey@outlook.com>
2021-07-14 12:10:01 +0300

    add the ability to like

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :description, presence: true, length: { maximum: 100 }
   has_many :comments, dependent: :delete_all
+  has_many :likes, dependent: :delete_all
+
   include ImageUploader::Attachment(:image) # adds an `image` virtual attribute
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_13_145055) do
+ActiveRecord::Schema.define(version: 2021_07_14_081007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2021_07_13_145055) do
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "following_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "posts", force: :cascade do |t|
