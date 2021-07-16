773bc165c89d8399e56840928542305fb220f11f
Andrei <litvinov.andrey@outlook.com>
2021-07-16 13:00:07 +0300

    added display of posts to whom subscribed to on the main page

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,10 @@
 class PublicController < ApplicationController
   def home
-    @posts = Post.limit(15)
+    @user = current_user
+    @selected_posts = []
+    # @selected_posts = Post.where { |post| @user.followings.include? post.user_id } unless @user.nil?
+    @selected_posts = Post.where(user_id: @user.followings.pluck(:id)).order(created_at: :desc)
+    @new_posts = Post.limit(15).order(created_at: :desc)
+
   end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class ImageUploader < Shrine
   plugin :default_url
   plugin :validation_helpers
 
-  Attacher.default_url do |**options|
-    "/placeholders/frog.gif"
+  Attacher.default_url do |**_options|
+    '/placeholders/a!.jpg'
   end
 
   Attacher.validate do
