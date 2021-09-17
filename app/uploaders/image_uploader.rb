8d54d86b8d53b63295f5131a7fc38f98736d4a18
fennec <litvinov.andrey@outlook.com>
2021-09-17 22:17:30 +0300

    something new

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,10 +5,10 @@ class ImageUploader < Shrine
   plugin :validation_helpers
 
   Attacher.default_url do |**_options|
-    '/placeholders/frog.gif'
+    'otter by fen.png'
   end
 
   Attacher.validate do
-    validate_extension %w[jpeg png gif]
+    validate_extension %w[jpeg png gif jpg]
   end
 end
