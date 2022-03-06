08a41aa9c2ba79e870c3b60910325d666cc99581
Alexander Pavlyuk <alexander.pavlyuk@jetruby.com>
2022-03-06 04:08:05 +0300

    Add design for main page and post page

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,10 +2,14 @@
 
 class PublicController < ApplicationController
   def home
+    user_followeds = current_user.being_followeds.pluck(:id)
+
     unless current_user.nil?
-      @selected_posts = Post.where(user_id: current_user.followers.pluck(:id)).order(created_at: :desc)
+      @selected_posts = Post.where(user_id: user_followeds).order(created_at: :desc)
     end
     @new_posts = Post.limit(15).order(created_at: :desc)
-    @new_posts = Post.where.not(user_id: current_user.id).limit(15).order(created_at: :desc) unless current_user.nil?
+    @new_posts = Post.where.not(
+      user_id: (user_followeds << current_user.id)
+    ).limit(15).order(created_at: :desc) unless current_user.nil?
   end
 end
