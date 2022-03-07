4b9c140c38983198cf5c87a4f8919b62940229d5
Alexander Pavlyuk <alexander.pavlyuk@jetruby.com>
2022-03-07 04:11:50 +0300

    add gem and resolve bugs

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,15 +2,15 @@
 
 class PublicController < ApplicationController
   def home
-    unless current_user.nil?
+    if current_user.present?
       user_followeds = current_user.being_followeds.pluck(:id)
+
       @selected_posts = Post.where(user_id: user_followeds).order(created_at: :desc)
-    end
-    @new_posts = Post.limit(15).order(created_at: :desc)
-    unless current_user.nil?
       @new_posts = Post.where.not(
         user_id: (user_followeds << current_user.id)
-      ).limit(15).order(created_at: :desc)
+      ).limit(15).order(created_at: :desc) unless current_user.nil?
     end
+    @new_posts = Post.limit(15).order(created_at: :desc)
+
   end
 end
