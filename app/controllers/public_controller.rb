8d40eefe2862ee23ab8cb9fdd5a6628bb5b6dd3f
andrey.litvinov <andrey.litvinov@jetruby.com>
2022-03-07 11:38:48 +0300

    hot fix

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,15 +2,15 @@
 
 class PublicController < ApplicationController
   def home
-    if current_user.present?
+    unless current_user.nil?
       user_followeds = current_user.being_followeds.pluck(:id)
-
       @selected_posts = Post.where(user_id: user_followeds).order(created_at: :desc)
-      @new_posts = Post.where.not(
-        user_id: (user_followeds << current_user.id)
-      ).limit(15).order(created_at: :desc) unless current_user.nil?
     end
     @new_posts = Post.limit(15).order(created_at: :desc)
-
+    unless current_user.nil?
+      @new_posts = Post.where.not(
+        user_id: (user_followeds << current_user.id)
+      ).limit(15).order(created_at: :desc)
+    end
   end
 end
